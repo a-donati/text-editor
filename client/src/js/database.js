69f8415bce947797ab8278ff12f4d7479c2c44fb
Angela Donati <angeladonati93@gmail.com>
2022-05-03 18:26:05 -0400

    update putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,8 +12,22 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+//  accept content and add it to the database
+export const putDb = async (content) => {
+  console.log('Post to the database');
+  // create connection to db
+  const textDb = await openDB('jate', 1);
+  // create new transaction with readwrite privledges
+  const tx = textDb.transaction('jate', 'readwrite');
+  // open desired object store
+  const store = tx.objectStore('jate');
+  // use .add() method on store to pass in the content
+  const request = store.add(content);
+  // receive confirmation
+  const result = await request;
+  console.log(`Data saved to database: ${result}`)
+}
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
